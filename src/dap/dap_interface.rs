8d471dbeced11c2084aa5b5e36461f78d70e2b51
Gustavo Moitinho Trindade <gmoitinhotrindade@gmail.com>
2025-06-29 10:33:33 -0300

    Implement setting breakpoints in DAP via interface

--- src/dap/dap_interface.rs
+++ src/dap/dap_interface.rs
@@ -1,7 +1,9 @@
+use crate::dap::message::{DapEvent, ProtocolMessage, RequestMessage, ResponseMessage, SetBreakpointsArguments};
+use crate::dap::{DapError, DapInstance};
+use crate::dap::message_types;
+use crate::data::breakpoints::{Breakpoint, BreakpointStore};
 use std::path::{Path, PathBuf};
 use std::sync::{Arc, RwLock};
-use crate::dap::{DapError, DapInstance};
-use crate::data::breakpoints::{Breakpoint, BreakpointStore};
 
 type ProtectedOption<T> = Arc<RwLock<Option<T>>>;
 
@@ -17,61 +19,132 @@ impl DapInterface {
             breakpoints: BreakpointStore::new(),
         }
     }
-    
+
     pub fn load_target(&self, filepath: impl AsRef<Path>) -> Result<(), DapError> {
         let instance = DapInstance::instance(filepath)?;
         let mut w_dap = self.instance.write().unwrap();
+        tracy_client::Client::start().message("load_target_instance_w", 0);
+
         *w_dap = Some(instance);
         Ok(())
     }
-    
+
     pub fn launch(&self, launch_json: impl AsRef<str>) -> Result<(), DapError> {
         let mut w_dap = self.instance.write().unwrap();
+        tracy_client::Client::start().message("launch_instance_w", 0);
         if let Some(w_dap) = &mut *w_dap {
             w_dap.launch(launch_json.as_ref())?;
-            Ok(())
         } else {
-            Err(DapError::NoLoadedTarget)
+            return Err(DapError::NoLoadedTarget)
         }
+        Ok(())
     }
-    
-    pub fn put_breakpoint(&self, breakpoint: Breakpoint) {
+
+    pub fn process_dap_events(&self) -> Result<(), DapError> {
+        let mut configuration_done = false;
+        
+        {
+            let mut instance_w = self.instance.write().unwrap();
+            tracy_client::Client::start().message("process_dap_events_instance_w", 0);
+
+            if let Some(dap_interface) = &mut *instance_w {
+                while let Some(msg) = dap_interface.poll_message() {
+                    log::debug!("Received message: {msg:?}");
+                    match msg {
+                        ProtocolMessage::Response(ResponseMessage::Initialize { success, .. }) => {
+                            if success {
+                                configuration_done = true;
+                            } else {
+                                log::error!("Failed to initialize DAP");
+                            }
+                        }
+                        ProtocolMessage::Event(DapEvent::Terminated { .. }) => {}
+                        _ => {}
+                    }
+                }
+            }
+        }
+        
+        if configuration_done {
+            self.update_all_breakpoints()?;
+
+            {
+                let mut instance_w = self.instance.write().unwrap();
+                if let Some(dap_interface) = instance_w.as_mut() {
+                    let seq = dap_interface.next_seq();
+                    dap_interface
+                        .send_message(&ProtocolMessage::Request(
+                            RequestMessage::ConfigurationDone {
+                                seq,
+                                arguments: None,
+                            },
+                        ))?;
+                }
+            }
+        }
+        
+        Ok(())
+    }
+
+    pub fn update_all_breakpoints(&self) -> Result<(), DapError> {
+        let mut files = Vec::new();
+        self.breakpoints.get_files(&mut files);
+
+        for file in &files {
+            self.update_breakpoints_for_file(file)?
+        }
+
+        Ok(())
+    }
+
+    fn update_breakpoints_for_file(&self, file: &Path) -> Result<(), DapError> {
+        let mut instance_w = self.instance.write().unwrap();
+        tracy_client::Client::start().message("update_breakpoints_for_file_instance_w", 0);
+        if let Some(instance) = instance_w.as_mut() {
+            let mut list = Vec::new();
+            self.breakpoints.get_file_breakpoints(file, &mut list);
+            let source = message_types::Source {
+                path: Some(file.to_string_lossy().into()),
+                ..Default::default()
+            };
+            let breakpoints = list.iter().map(|bp| {
+                message_types::SourceBreakpoint {
+                    line: bp.lineno,
+                    ..Default::default()
+                }
+            }).collect();
+
+            let seq = instance.next_seq();
+            let msg = ProtocolMessage::Request(RequestMessage::SetBreakpoints {
+                seq,
+                arguments: SetBreakpointsArguments {
+                    source,
+                    breakpoints: Some(breakpoints),
+                    ..Default::default()
+                },
+            });
+
+            instance.send_message(&msg)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn put_breakpoint(&self, breakpoint: Breakpoint) -> Result<(), DapError> {
         self.breakpoints.add(breakpoint.clone());
+        self.update_breakpoints_for_file(&breakpoint.file)
     }
-    
-    pub fn remove_breakpoint(&self, breakpoint: &Breakpoint) {
+
+    pub fn remove_breakpoint(&self, breakpoint: &Breakpoint) -> Result<(), DapError> {
         self.breakpoints.remove(&breakpoint);
+        self.update_breakpoints_for_file(&breakpoint.file)
     }
-    
+
     pub fn get_files_with_breakpoints(&self, out: &mut Vec<PathBuf>) {
         self.breakpoints.get_files(out);
     }
-    
+
     pub fn get_file_breakpoints(&self, file: impl AsRef<Path>, out: &mut Vec<Breakpoint>) {
         self.breakpoints.get_file_breakpoints(file, out);
     }
 }
-//if let Some(dap_interface) = dap_instance {
-// while let Some(msg) = dap_interface.poll_message() {
-// log::debug!("Received message: {msg:?}");
-// match msg {
-// ProtocolMessage::Response(ResponseMessage::Initialize { success, .. }) => {
-// if success {
-// let seq = dap_interface.next_seq();
-// dap_interface.send_message(
-// &ProtocolMessage::Request(RequestMessage::ConfigurationDone {
-// seq,
-// arguments: None,
-// }),
-// ).unwrap()
-// } else {
-// log::error!("Failed to initialize DAP");
-// }
-// }
-// ProtocolMessage::Event(DapEvent::Terminated { .. }) => {
-// self.debugging = false;
-// }
-// _ => {}
-// }
-// }
-// }
